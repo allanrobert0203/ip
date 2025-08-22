72cc5ae8dd9114cef73c447062c31399601397f9
Allan Robert <johnallanrobert@gmail.com>
2025-08-22 10:48:30 +0800

    Added echoing functionality

--- src/main/java/Chitti.java
+++ src/main/java/Chitti.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Chitti {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Chitti \nWhat can I do for you? \nBye. Hope to see you again soon!");
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Chitti \nWhat can I do for you?");
+        System.out.println("---------------------------");
+        String echo = myScanner.nextLine();
+
+        while (!echo.equals("bye")) {
+            System.out.println(echo);
+            System.out.println("---------------------------");
+            echo = myScanner.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("---------------------------");
+        myScanner.close();
+
     }
 }
