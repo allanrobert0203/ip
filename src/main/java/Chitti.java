1193429802481400064745074a26de53441e7ab7
Allan Robert <johnallanrobert@gmail.com>
2025-08-22 13:44:04 +0800

    added mark and unmark functionality

--- src/main/java/Chitti.java
+++ src/main/java/Chitti.java
@@ -3,28 +3,41 @@ import java.util.Scanner;
 public class Chitti {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int counter = 0;
 
-        System.out.println("Hello! I'm Chitti \nWhat can I do for you?");
+        System.out.println("Hello! I'm Chitti the robot. Speed 1 terahertz, memory 1 zigabyte.\nWhat can I do for you?");
+        System.out.println("(Commands: 'list', 'mark <number>', 'unmark <number>', 'bye')");
         System.out.println("---------------------------");
         String echo = myScanner.nextLine();
 
-        while (!echo.equals("bye") ) {
-            if (!echo.equals("list")) {
-                System.out.println("Added: " + echo);
-                System.out.println("---------------------------");
-                list[counter] = echo;
-                counter++;
-                echo = myScanner.nextLine();
-            } else {
+        while (!echo.equals("bye")) {
+            if (echo.equals("list")) {
                 for (int i = 0; i < counter; i++) {
                     int order = i + 1;
-                    System.out.println(order + ". " + list[i]);
+                    System.out.println(order + ". " + list[i].toString());
                 }
-                System.out.println("---------------------------");
-                echo = myScanner.nextLine();
+            } else if (echo.startsWith("mark ")) {
+                String[] parts = echo.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                list[taskIndex].markAsDone();
+
+                System.out.println("Great job! I have marked this task as done!");
+                System.out.println("\t" + list[taskIndex]);
+            } else if (echo.startsWith("unmark ")) {
+                String[] parts = echo.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+                list[taskIndex].markAsNotDone();
+
+                System.out.println("Awwww, I've marked this task as not done yet:");
+                System.out.println("\t" + list[taskIndex]);
+            } else {
+                System.out.println("Added: " + echo);
+                list[counter] = new Task(echo);
+                counter++;
             }
+            System.out.println("---------------------------");
+            echo = myScanner.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
