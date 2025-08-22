2f63f47b880d896013597b7d032b32ef7eb2df66
Allan Robert <johnallanrobert@gmail.com>
2025-08-22 10:07:02 +0800

    Changed chatbot's name

---
+++ src/main/java/Chitti.java
@@ -0,0 +1,5 @@
+public class Chitti {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Chitti \nWhat can I do for you? \nBye. Hope to see you again soon!");
+    }
+}
