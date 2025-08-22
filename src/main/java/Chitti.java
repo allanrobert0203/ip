ad36c9c1d3c9e49efe02dde6f99b70a913f8508f
Allan Robert <johnallanrobert@gmail.com>
2025-08-22 11:10:51 +0800

    Added list functionality

--- src/main/java/Chitti.java
+++ src/main/java/Chitti.java
@@ -3,15 +3,28 @@ import java.util.Scanner;
 public class Chitti {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
 
         System.out.println("Hello! I'm Chitti \nWhat can I do for you?");
         System.out.println("---------------------------");
         String echo = myScanner.nextLine();
 
-        while (!echo.equals("bye")) {
-            System.out.println(echo);
-            System.out.println("---------------------------");
-            echo = myScanner.nextLine();
+        while (!echo.equals("bye") ) {
+            if (!echo.equals("list")) {
+                System.out.println("Added: " + echo);
+                System.out.println("---------------------------");
+                list[counter] = echo;
+                counter++;
+                echo = myScanner.nextLine();
+            } else {
+                for (int i = 0; i < counter; i++) {
+                    int order = i + 1;
+                    System.out.println(order + ". " + list[i]);
+                }
+                System.out.println("---------------------------");
+                echo = myScanner.nextLine();
+            }
         }
 
         System.out.println("Bye. Hope to see you again soon!");
